ad5166ff0c342e37e7249eb13830af8d8ddd58ce
admin <chiinhuv@gmail.com>
2018-11-14 14:47:32 +0700

    add

--- RadioButtons/ViewController.swift
+++ RadioButtons/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //keo outlet cua ca 3 button (male,female,other) vao cung mot outlet gender trong kieu array button
+    @IBOutlet var genderButtons: [UIButton]!
+    @IBOutlet weak var save: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        save.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func selectedGender(_ sender: UIButton) {
+        for button in genderButtons {
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        save.isEnabled = true
+    }
+    
 
 }
 
